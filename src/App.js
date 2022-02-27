0f2ba2cec43ab52415801050da9de2d28d2ca64d
Martin Gs <martings81@gmail.com>
2022-02-26 22:39:22 -0300

    Mitad de clase, cambios al nav
    Nav desde 0 sin bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import NavBar from './components/NavBar.js';
+import Main from './components/Main.js';
 import ItemListContainer from './components/ItemListContainer';
 
 function App() {
     return (
         <>
             <Header />
+            <Main />
             <ItemListContainer />
             <Footer />
         </>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,16 @@
-import CardWidget from './CardWidget';
-
+/* import CardWidget from './CardWidget';
+ */
 function NavBar() {
     return (
         <>
-            <CardWidget />
+            <nav>
+                <ul>
+                    <li>Vínculo 1</li>
+                    <li>Vínculo 2</li>
+                    <li>Vínculo 3</li>
+                </ul>
+            </nav>
+            {/* <CardWidget /> */}
         </>
     );
 }
