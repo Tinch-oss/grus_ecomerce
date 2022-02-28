a294b5cdc6bc2d9a6499ff4aeab262776f8ce89a
Martin Gs <martings81@gmail.com>
2022-02-28 12:33:51 -0300

    Monopatín Eléctrico Grus 0.35
    Se agrega un icono .png al componente CartWidget

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,5 +1,11 @@
+import companyLogo from '../media/shopping-cart.png';
+
 function CartWidget() {
-    return;
+    return (
+        <>
+            <img src={companyLogo} alt="Icono de un carrito de compras" />
+        </>
+    );
 }
 
 export default CartWidget;
