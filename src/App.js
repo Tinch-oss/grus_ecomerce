124589420ca73e8d4435f739f083c822c9f99cce
Martin Gs <martings81@gmail.com>
2022-02-28 12:17:39 -0300

    Revert "Monopatín Eléctrico Grus 0.3"
    This reverts commit 1ea2d7c5ad95d726e22334611ef191b99f1d9412.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ItemListContainer from './components/ItemListContainer';
 
 function App() {
     const nombre = 'Martin';
-    const apellido = 'Grus';
+    const apellido = 'Martin';
     return (
         <>
             <Header />
